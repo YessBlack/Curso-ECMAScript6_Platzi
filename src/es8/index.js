569a2da7d200d27a6c0666e42ea34572d2809375
Angelica Garcia <70681219+YessBlack@users.noreply.github.com>
2022-04-23 13:34:07 -0500

    practica clases

---
+++ src/es8/index.js
@@ -0,0 +1,23 @@
+const data = {
+    frontend:'Angelica',
+    backend:'Rosa',
+    design:'Nieves'
+}
+
+const entries = Object.entries(data)
+console.log(entries)
+
+const data1 = {
+    frontend:'Angelica',
+    backend:'Rosa',
+    design:'Nieves',
+}
+
+const values = Object.values(data1)
+console.log(values)
+console.log(values.length)
+
+const string = 'Hello'
+
+console.log(string.padStart(8,'hi '))
+//para presentar una estructura de elementos como lista de elementos o productos
